8570e20b7a6a3488022e8dc53037e61e9639ffd2
Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2018-10-10 15:31:42 -0400

    Integrete with Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBsuGweeRiZSUTVsBz3mFLpn7otuCjoqVE",
+    authDomain: "bulletinboard-dc582.firebaseapp.com",
+    databaseURL: "https://bulletinboard-dc582.firebaseio.com",
+    projectId: "bulletinboard-dc582",
+    storageBucket: "bulletinboard-dc582.appspot.com",
+    messagingSenderId: "483974435376"
+  };
 
 @Component({
   selector: 'app-root',
@@ -22,5 +31,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
