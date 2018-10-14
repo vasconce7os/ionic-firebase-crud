42c66b12b90173a0f6a45023e75a90738501abc4
Vasconcelos <cerebro.vasconcelos@zipmail.com.br>
2018-10-14 17:59:19 -0400

    Implement List

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  infos = [];
+  ref = firebase.database().ref('infos/');
 
+  constructor(public router: Router) {
+    this.ref.on('value', resp => {
+      this.infos = [];
+      this.infos = snapshotToArray(resp);
+    });
+  }
 }
+
+export const snapshotToArray = snapshot => {
+  let returnArr = [];
+
+  snapshot.forEach(childSnapshot => {
+      let item = childSnapshot.val();
+      item.key = childSnapshot.key;
+      returnArr.push(item);
+  });
+
+  return returnArr;
+};
